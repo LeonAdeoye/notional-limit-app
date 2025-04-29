931c3f4e51179539e7147c4f93d47d2c118490a4
LeonAdeoye <leon.adeoye@gmail.com>
2025-04-29 22:59:00 +0800

    added notional breaches tab

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import TraderNotionalGrid from "./TraderNotionalGrid";
 import {useState} from "react";
 import {Box, Tab} from "@mui/material";
 import {TabContext, TabList, TabPanel} from "@mui/lab";
+import NotionalBreachesGrid from "./NotionalBreachesGrid";
 
 const App = () => {
 
@@ -18,9 +19,9 @@ const App = () => {
               <TabContext value={tabValue}>
                   <Box>
                       <TabList centered onChange={handleChange} textColor='primary' indicatorColor='primary'>
-                          <Tab sx={{backgroundColor:'lightgray', textTransform: 'capitalize', fontWeight: 'bold', border: '1px solid gray', borderTopRightRadius: '8px', borderTopLeftRadius: '8px'}} label='Desk Notional' value='1'/>
-                          <Tab sx={{backgroundColor:'lightgray', textTransform: 'capitalize', fontWeight: 'bold', border: '1px solid gray', borderTopRightRadius: '8px', borderTopLeftRadius: '8px'}} label='Trader Notional' value='2'/>
-
+                          <Tab sx={{backgroundColor:'lightgray', textTransform: 'capitalize', fontWeight: 'bold', border: '1px solid gray', borderTopRightRadius: '8px', borderTopLeftRadius: '8px'}} label='Desk Notionals' value='1'/>
+                          <Tab sx={{backgroundColor:'lightgray', textTransform: 'capitalize', fontWeight: 'bold', border: '1px solid gray', borderTopRightRadius: '8px', borderTopLeftRadius: '8px'}} label='Trader Notionals' value='2'/>
+                          <Tab sx={{backgroundColor:'lightgray', textTransform: 'capitalize', fontWeight: 'bold', border: '1px solid gray', borderTopRightRadius: '8px', borderTopLeftRadius: '8px'}} label='Notional Breaches' value='3'/>
                       </TabList>
                   </Box>
                   <TabPanel value='1'>
@@ -31,6 +32,11 @@ const App = () => {
                   <TabPanel value='2'>
                       <div>
                           <TraderNotionalGrid/>
+                      </div>
+                  </TabPanel>
+                  <TabPanel value='3'>
+                      <div>
+                          <NotionalBreachesGrid/>
                       </div>
                   </TabPanel>
               </TabContext>
---
+++ src/NotionalBreachesGrid.tsx
@@ -0,0 +1,51 @@
+import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
+import { AgGridReact } from 'ag-grid-react';
+import {useEffect, useState} from "react";
+import {DeskNotionalInterface} from "./DeskNotionalInterface";
+import {ColDef} from "ag-grid-community";
+import {updateDeskNotional} from "./orderNotionalSlice";
+import {useDispatch, useSelector} from "react-redux";
+import {OrderNotionalService} from "./OrderNotionalService";
+
+ModuleRegistry.registerModules([AllCommunityModule]);
+
+const DeskNotionalGrid = () => {
+    const dispatch = useDispatch();
+    const deskData: DeskNotionalInterface[] = useSelector((state) => state.orderNotional.deskOrderNotionals);
+
+    const onMessage = ({data, header}:{ data: DeskNotionalInterface, header: any }): void => {
+        if(header.command() === "p" || header.command() === "sow")
+            dispatch(updateDeskNotional(data));
+    }
+
+    const [orderNotionalService] = useState<OrderNotionalService | null>
+    (new OrderNotionalService("desk.notional.update","ws://localhost:9008/amps/json", onMessage));
+
+    useEffect(() => {
+        orderNotionalService?.connect().then(() => {
+            console.log("Connected to AMPS");
+        }).catch((error) => {
+            console.error("Error connecting to AMPS: " + error);
+        });
+        return () => orderNotionalService?.disconnect();
+    }, []);
+
+    const [columnDefs] = useState<ColDef<DeskNotionalInterface>[]>([
+        { headerName: 'DeskName', field: 'deskName', filter: true},
+        { headerName: 'Desk ID', field: 'deskId', hide: true},
+        { headerName: 'Buy Notional Limit', field: 'buyNotionalLimit' },
+        { headerName: 'Current Buy Notional', field: 'currentBuyNotional' },
+        { headerName: 'Sell Notional Limit', field: 'sellNotionalLimit' },
+        { headerName: 'Current Sell Notional', field: 'currentSellNotional' },
+        { headerName: 'Gross Notional Limit', field: 'grossNotionalLimit' },
+        { headerName: 'Current Gross Notional', field: 'currentGrossNotional' },
+    ]);
+
+    return (
+        <div style={{ marginTop: 10, marginLeft: 10, height: 500, width: '99%'}}>
+            <AgGridReact rowData={deskData} columnDefs={columnDefs}/>
+        </div>
+    );
+}
+
+export default DeskNotionalGrid;
